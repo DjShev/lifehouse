f85278e9a6b60040323c612dc11f7499061487c5
GinaShev <ginashev@gmail.com>
2018-11-06 15:28:13 +0100

    Update LoginPage.java

--- src/main/java/automation/page/LoginPage.java
+++ src/main/java/automation/page/LoginPage.java
@@ -11,22 +11,22 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class LoginPage extends Page {
-    static String url = "http://staging-prelaunch.life-house.com";
+    static String url = "http://staging-prelaunch.life-house.com"; // url address should be move to environment.properties 
     static SelenideElement emailField = $(By.name("email"));
-    static SelenideElement insideButton = $(By.id("index-step-inside"));
+    static SelenideElement insideButton = $(By.id("index-step-inside")); // no need by.id - need to wrire #index-step-inside
     static SelenideElement passwordField = $(By.name("password"));
-    static SelenideElement insidePopUPButton = $(By.id("index-sign-up-popup-step-inside"));
+    static SelenideElement insidePopUPButton = $(By.id("index-sign-up-popup-step-inside")); //no need by.id
 
-    public static void openPage() {
+    public static void openPage() { //should be commone method
         open(url);
     }
 
-    public static void enterEmail() {
+    public static void enterEmail() {   //name should be change to EnterEmail and press enter or make separete method
         emailField.setValue(getEmailAddress());
         insideButton.pressEnter();
     }
 
-    public static void enterPassword() {
+    public static void enterPassword() {     //name should be change to enterPassword and press enter or make separete method
         passwordField.setValue(getPassword());
         insidePopUPButton.pressEnter();
     }

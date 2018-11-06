ca1ac17c2319249ac1b41f0b8ac4f4983482ea4a
GinaShev <ginashev@gmail.com>
2018-11-06 15:24:09 +0100

    Update MainPage.java

--- src/main/java/automation/page/MainPage.java
+++ src/main/java/automation/page/MainPage.java
@@ -8,7 +8,10 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class MainPage extends Page {
 
-    static SelenideElement headerText = $(By.cssSelector(".share__holder h1"));
+    static SelenideElement headerText = $(By.cssSelector(".share__holder h1"));  //not sure thet need to use By.cssSelector 
+                                                                                 //-$(String cssSelector) – returns object of 
+                                                                 //the SelenideElement class that represents first 
+                                                               //element found by CSS selector on the page.
 
     public static void checkHeader() {
         headerText.should(Condition.text("Friends\nWith Benefits"));

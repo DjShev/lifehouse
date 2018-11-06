e2eedc726ee26d18cc4fed9e8357fff006a9772d
GinaShev <ginashev@gmail.com>
2018-11-06 15:31:24 +0100

    Update randomCustomerData.java

--- src/main/java/automation/randomData/randomCustomerData.java
+++ src/main/java/automation/randomData/randomCustomerData.java
@@ -10,12 +10,12 @@ public class randomCustomerData {
     private static Random r = new Random();
 
 
-    static public String getEmailAddress() {
+    static public String getEmailAddress() {   //specify lenght of email field
         dataFactory.randomize(r.nextInt());
         return dataFactory.getEmailAddress();
     }
 
-    static public String getPassword() {
+    static public String getPassword() {         
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?";
         return RandomStringUtils.random(6, characters);
     }
